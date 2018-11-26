93dd82d5cf1726d0509a909cf9ac0961e7a2b187
Mantas Sasnauskas <man_tas_pats@yahoo.com>
2018-11-26 20:12:32 +0200

    tvarkomas portfolio. reik dadet JS del filtro. Taip pat img failam priskirt html. Kontaktai sutvarkyti su grid. Sutvarkytas Google maps su iframe. Padarytas JS skaiciutuvas 2018.11.21 Masyvo spausdinimas i puslapi su JS. 2018.11.22 lempute in progress. jQuery AJAX pridetas meniu baras portfolio.

--- JS/jQuery-AJAX.js
+++ JS/jQuery-AJAX.js
@@ -1,7 +1,15 @@
-var meniu = [{
-    "title":"Pradzia",
-    "url":"/"},{
-    "title":"Portfolio",
-    "url":"portfolio.html"},{
-    "title":"Kontaktai",
-    "url":"kontaktai.html"}];
+$(document).ready(function () {
+    var meniu = '';
+    $.get("/JS/meniuItems.json", function (data, status) {
+        console.log(status);
+        console.log(data);
+        meniu = data;
+        var links = '';
+        for (var i = 0; i < meniu.length; i++) {
+            var titlas = meniu[i].title;
+            var hrefas = meniu[i].url;
+            links += "<a href='" + hrefas + "'>" + titlas + "</a>";
+        }
+        $('.top').html(links);
+    });
+});
